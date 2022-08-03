ac66cf04011626a391233bbbeaa1e0b2e3f61da6
PAULO HENRIQUE MARGUTTI JUNIOR <paulo.hmjunior2@senacsp.edu.br>
2022-08-02 21:54:53 -0300

    alteracao 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://calm-reaches-52714.herokuapp.com',
     headers: {
         'Content-Type': 'application/json'
     },
